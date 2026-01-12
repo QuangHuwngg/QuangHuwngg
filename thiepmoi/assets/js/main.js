5089b7858f32db908bc6e555e874cb3a4a14b395
Nguyễn Quang Hưng <105227731+QuangHuwngg@users.noreply.github.com>
2026-01-12 17:35:31 +0700

    Delete thiepmoi directory

--- thiepmoi/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-    const doors = document.querySelector(".door-wrap");
-    setTimeout(() => {
-        doors.classList.add("open");
-        setTimeout(() => {
-            doors.style.display = "none";
-        }, 2800);
-    }, 500);
-});
